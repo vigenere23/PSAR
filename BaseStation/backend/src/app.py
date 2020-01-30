28b93ff64b8134e75d3e6e5780775be17f1c9506
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2020-01-30 08:47:24 -0500

    created context layer

--- BaseStation/backend/src/app.py
+++ BaseStation/backend/src/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask
+from flask_socketio import SocketIO
     
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-SocketIO()
 socketio = SocketIO(app, cors_allowed_origins="*", ping_timeout=1, ping_interval=1)
--- BaseStation/backend/src/main.py
+++ BaseStation/backend/src/main.py
@@ -1,17 +1,7 @@
 from .app import app, socketio
-from flask_socketio import emit
-from .interfaces.socket_api import TestNamespace
+from .context.interfaces import SocketApiContext
 
-@app.route('/')
-def index():
-  return (
-    'Yes, it works... yeeaahh... ' +
-    'Now try a socket connection and fire the event' +
-    '\'request_event\' while listening to a \'response_event\'' +
-    'and see if that works.'
-  )
-
-socketio.on_namespace(TestNamespace(''))
+SocketApiContext(socketio).register()
 
 if __name__ == '__main__':
   socketio.run(app)
