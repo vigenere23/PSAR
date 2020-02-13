fda301ebafcccc2a8577c36a77104b45d7d21d26
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2020-02-13 08:37:04 -0500

    added todos

---
+++ BaseStation/backend/src/domain/event_emitters/RobotEventEmitter.py
@@ -0,0 +1,11 @@
+class RobotEventEmitter:
+
+    def __init__(self, socketio):
+        self.__socketio = socketio
+
+    # TODO
+    # send_move()
+    # send_rotate()
+    # send_take_puck()
+    # send_release_puck()
+    # etc.
