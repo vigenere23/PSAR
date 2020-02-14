0ad246633eb0171957a0351ed4a10de7432851b3
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2020-02-14 16:33:44 -0500

    added documentation

--- BaseStation/backend/src/domain/sequence/Retryable.py
+++ BaseStation/backend/src/domain/sequence/Retryable.py
@@ -3,10 +3,24 @@ from .exceptions import RetryException
 
 class Retryable:
 
-    def __init__(self, number_of_times):
+    """
+    Decorator that makes a task retryable.
+
+    :param number_of_times:
+    :type int:
+        Number of times the task can retry.
+        This number is reset each time a task succeeds or if
+        the number of retries permitted has been reached.
+        Note that the task will be run (number_of_times + 1) times in total.
+    """
+    def __init__(self, number_of_times: int):
         self.__initial_retries_left = number_of_times
         self.__retries_left = number_of_times
 
+    """
+    :raise RetryException: if the task should be retried
+    :raise RuntimeError: if the number of retries has been exceeded
+    """
     def __call__(self, f):
         def wrapper(*args, **kwargs):
             if self.__retries_left >= 0:
