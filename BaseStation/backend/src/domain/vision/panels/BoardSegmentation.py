9a2b034c5dbb9ff8dabac2a7ff099738353ec332
fjlacroixUlaval <55036755+fjlacroixUlaval@users.noreply.github.com>
2020-03-01 21:58:36 -0500

    Scripts for Arrow Board Detection (#48)
    * Scripts for Arrow Board Detection
    
    * if name == main
    
    * if name == main

---
+++ BaseStation/backend/src/domain/vision/panels/BoardSegmentation.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+
+    pannel_path = "10004.jpg"
+    print(pannel_path)
+
+    img = cv2.imread(pannel_path)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    low_hsv = np.array([0, 40, 40])
+    high_hsv = np.array([255, 255, 255])
+    mask1 = cv2.inRange(img, low_hsv, high_hsv)
+
+    k_size = 20
+    kernel = np.ones((k_size,k_size),np.uint8)
+    opened = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, kernel)
+
+    k_size = 10
+    kernel = np.ones((k_size,k_size),np.uint8)
+    opened = cv2.erode(opened,kernel,iterations = 2)
+
+    k_size = 20
+    kernel = np.ones((k_size,k_size),np.uint8)
+    opened = cv2.morphologyEx(opened, cv2.MORPH_OPEN, kernel)
+
+    opened = cv2.bitwise_not(opened)
+
+    res1 = cv2.bitwise_and(img, img, mask=opened)
+
+    cv2.imwrite("output.png", res1)
